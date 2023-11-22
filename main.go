adfe12ee3d05979e6ef72a95952023725668a043
wesley-lewis <lewis.wes.23@gmail.com>
2023-11-22 23:10:36 +0530

    testing in a very bad way

--- main.go
+++ main.go
@@ -1,43 +1,35 @@
 package main
 
 import (
-	"net"
-	"log"
-	"time"
+	"flag"
+	// "net"
+	// "log"
 
 	"github.com/wesley-lewis/distributed-cache/cache"
 )
 
 
 func main() {
+	// conn, err := net.Dial("tcp", ":3000")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// _, err = conn.Write([]byte("SET Foo Bar 40000000"))
+	// return 
+	var (
+		listenAddr = flag.String("listenaddr", ":3000", "listen address of the server")
+		leaderAddr = flag.String("leaderaddr", "", "listen address of the leader")
+	)
+	flag.Parse()
+
 	opts := ServerOpts {
-		ListenAddr: ":3000",
-		IsLeader: true,
+		ListenAddr: *listenAddr,
+		IsLeader: len(*leaderAddr) == 0,
+		LeaderAddr: *leaderAddr,
 	}
-
-	go func() {
-		time.Sleep(time.Second * 2)
-		conn, err := net.Dial("tcp",":3000")
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		n, err := conn.Write([]byte("SET Foo Bar 34000000000"))
-		if err != nil {
-			log.Fatal(err)
-		}
-		log.Printf("Wrote %d bytes\n", n)
-
-		time.Sleep(time.Second * 2)
-		conn.Write([]byte("GET Foo"))
-		buf := make([]byte, 1024)
-		n, err = conn.Read(buf)
-		if err != nil {
-			log.Fatal(err)
-		}
-		log.Printf("Read %d bytes\n", n)
-		log.Println("Value:", string(buf[:n]))
-	}()
 
 	server := NewServer(opts, cache.New())
 	server.Start()
