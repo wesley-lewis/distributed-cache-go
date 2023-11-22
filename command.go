6c87d8df7c09eed2efe14a0213fbd9574b998839
wesley-lewis <lewis.wes.23@gmail.com>
2023-11-22 19:21:06 +0530

    parsing command

--- command.go
+++ command.go
@@ -2,6 +2,11 @@ package main
 
 import (
 	"time"
+	"strings"
+	"errors"
+	"strconv"
+	"fmt"
+	"log"
 )
 
 type Command string 
@@ -28,3 +33,33 @@ type Message struct {
 	TTL time.Duration
 }
 
+func parseCommand(raw []byte) (*Message, error) {
+	var (
+		rawStr = string(raw)
+		parts = strings.Split(rawStr, " ")
+	)
+	if len(parts) < 2 {
+		// respond
+		log.Println("invalid command")
+		return nil, fmt.Errorf("invalid protocol format")
+	}
+
+	msg := &Message {
+		Cmd: Command(parts[0]),
+		Key: []byte(parts[1]),
+	}
+	
+	if msg.Cmd == CMDSet {
+		if len(parts) == 4 {
+			return nil, errors.New("invalid SET command")
+		}
+		msg.Value = []byte(parts[2])
+		ttl, err := strconv.Atoi(parts[3])
+		if err != nil {
+			return nil, errors.New("invalid TTL field")
+		}
+		msg.TTL = time.Duration(ttl)
+		
+	}
+	return msg, nil
+}
