994925e0e22482ccc921a1b5212072d5f817944d
wesley-lewis <lewis.wes.23@gmail.com>
2023-11-22 19:44:11 +0530

    bug fix: RUnlock instead of Unlock

--- cache/cache.go
+++ cache/cache.go
@@ -36,7 +36,7 @@ func (c *Cache) Has(key []byte) bool {
 
 func(c *Cache) Get(key []byte) ([]byte, error ) {
 	c.lock.RLock()
-	defer c.lock.Unlock()
+	defer c.lock.RUnlock()
 	
 	keyStr := string(key)
 	val, ok := c.data[keyStr]
@@ -51,13 +51,12 @@ func (c *Cache) Set(key, value []byte, ttl time.Duration) error {
 	c.lock.Lock()
 	defer c.lock.Unlock()
 	
-	ticker := time.NewTicker(ttl)
 	go func() {
-		<- ticker.C 
+		<- time.After(ttl)
 		delete(c.data, string(key))
 	}()
 
 	c.data[string(key)] = value
-
+	fmt.Printf("SET %s to %s\n", string(key), string(value))
 	return nil
 }
