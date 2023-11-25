c3eec3f6a753eba95dd84f6c0dc414b3f5c29b08
wesley-lewis <lewis.wes.23@gmail.com>
2023-11-25 14:04:43 +0530

    parsing test command

--- protocol.go
+++ protocol.go
@@ -24,18 +24,46 @@ type CommandSet struct {
 func(c *CommandSet) Bytes() []byte{
 	buf := new(bytes.Buffer)
 	binary.Write(buf, binary.LittleEndian, CmdSet)
-	binary.Write(buf, binary.LittleEndian, int64(len(c.Key)))
+	binary.Write(buf, binary.LittleEndian, int32(len(c.Key)))
 	binary.Write(buf, binary.LittleEndian, c.Key)	
 
-	binary.Write(buf, binary.LittleEndian, int64(len(c.Value)))
+	binary.Write(buf, binary.LittleEndian, int32(len(c.Value)))
 	binary.Write(buf, binary.LittleEndian, c.Value)	
 
-	binary.Write(buf, binary.LittleEndian, c.TTL)	
+	binary.Write(buf, binary.LittleEndian, int32(c.TTL))
 
 	return buf.Bytes()
 
 }
 
-func ParseCommand(r io.Reader) {
+func ParseCommand(r io.Reader) any{
+	var cmd Command 
+	binary.Read(r, binary.LittleEndian, &cmd)
+	switch cmd {
+	case CmdSet:
+		return parseSetCommand(r)
+	
+	default:
+		panic("invalid command")
+	}
+}
 
+func parseSetCommand(r io.Reader) *CommandSet {
+	cmd := &CommandSet{}
+	
+	var keyLen int32
+	binary.Read(r, binary.LittleEndian, &keyLen)
+	cmd.Key = make([]byte, keyLen)
+	binary.Read(r, binary.LittleEndian, &cmd.Key)
+
+	var valueLen int32
+	binary.Read(r, binary.LittleEndian, &valueLen)
+	cmd.Value = make([]byte, valueLen)
+	binary.Read(r, binary.LittleEndian, &cmd.Value)
+
+	var ttl int32
+	binary.Read(r, binary.LittleEndian, &ttl)
+	cmd.TTL = int(ttl)
+
+	return cmd
 }
