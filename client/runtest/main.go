61a826ba22e63600dc4739aa274a9ef1153f6c72
wesley-lewis <lewis.wes.23@gmail.com>
2023-11-28 01:19:02 +0530

    added raft library

--- client/runtest/main.go
+++ client/runtest/main.go
@@ -4,13 +4,51 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"net"
+	"os"
 	"time"
 
+	"github.com/hashicorp/raft"
 	"github.com/wesley-lewis/distributed-cache/client"
 )
 
+type Server struct {
+	raft *raft.Raft
+}
+
 func main() {
-	sendStuff()
+	var (
+		cfg				= raft.DefaultConfig()
+		fsm				= &raft.MockFSM{}
+		logStore		= &raft.InmemStore{}
+		snapShotStore	= raft.NewInmemSnapshotStore()
+		stableStore		= raft.NewInmemStore()
+		timeout			= time.Duration(time.Second * 5)
+	)
+	
+	cfg.LocalID = "ID"
+	ips, err := net.LookupIP("localhost")
+	if err != nil {
+		log.Fatal(err)
+	}
+	if len(ips) == 0 {
+		log.Fatalf("localhost did not resolve to any IPs")
+	}
+	addr := &net.TCPAddr{IP: ips[0], Port: 4000}
+
+	tr, err := raft.NewTCPTransport(":4000", addr, 10, timeout, os.Stdout)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	r, err := raft.NewRaft(cfg, fsm, logStore, stableStore, snapShotStore, tr)	
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("%+v", r)
+	select {
+	}
 }
 
 func sendStuff() {
